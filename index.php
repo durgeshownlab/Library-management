e92887fdbe5efeae1df9313abc460ccbd41cabb9
durgeshownlab <Durgeshownlab@gmail.com>
2022-06-08 15:45:06 +0530

    addding html structure of menubar

--- index.php
+++ index.php
@@ -6,12 +6,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
     <title>Home | Library Management</title>
 </head>
 <body>
     <div class="container">
         <div class="navbar">
+            <div class="logo">
+                <img src="img/logo.png" alt="logo">
+            </div>
+            <div class="menu-box">
+                <div class="menu-bar">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact us</a>
+                </div>
+            </div>
+            <div class="profile-box">
+                <img src="img/profile.png" alt="profile">
+            </div>
         </div>
     </div>
 </body>
